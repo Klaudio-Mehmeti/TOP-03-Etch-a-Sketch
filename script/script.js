81ecbc9c42146415dcd050eab07e06391f8ff6ad
Klaudio-Mehmeti <klaudio.mehmeti@hotmail.com>
2022-08-09 22:42:45 +0200

    USER GRID SIZE POP-UP FIX

--- script/script.js
+++ script/script.js
@@ -1,50 +1,105 @@
 
 
-let gridSide = parseInt(prompt("Enter a Grid Size", ""), 10);
-// gridSide = 20;
-const gridSquareQuantity = gridSide * gridSide;
+
+
+
 const container = document.querySelector('#grid-container');
-container.style.gridTemplateColumns = 'repeat(' + gridSide + ', auto)';
 const bwBtn = document.querySelector('.color-mode');
 const colorBtn = document.querySelector('.rainbow-mode');
 const eraserBtn = document.querySelector('.eraser');
+const gridNr = document.querySelector('.grid-number');
+const pencilMode = document.querySelector('.pencil-mode');
 
 
-for (i = 0; i < gridSquareQuantity; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.cssText = 'border-top: 1px solid rgb(160, 160, 160); border-left: 1px solid rgb(160, 160, 160);background: white;';
-    container.appendChild(square);
+gridNr.addEventListener('click', changeGrid);
 
-    bwMode(square);
-    colorMode(square);
-    eraser(square);
 
+// ------ DEFAULT GRID FUNCTION-----//
+
+function sketchPad(gridSize) {
+    for (i = 0; i < gridSize ** 2; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        container.style.gridTemplateColumns = 'repeat(' + gridSize + ', auto)';
+        pixel.style.cssText = 'border-top: 1px solid rgb(160, 160, 160); border-left: 1px solid rgb(160, 160, 160);background: white;';
+        container.appendChild(pixel);
+        bwMode(pixel);
+        colorMode(pixel);
+        eraser(pixel);
+        // pencil(pixel);
+    };
+};
+
+sketchPad(20);
+
+// ------ GRID SIZE BY USER-FUNCTION-----//
+
+function changeGrid() {
+    let gridNum = Number(prompt(' Enter the desired number of GRID!'));
+    while (gridNum > 100 || gridNum == '') {
+        gridNum = Number(prompt('must input a number less than 100'))
+
+    } if (gridNum <= 100) {
+        clearGrid();
+        sketchPad(gridNum);
+    }
 };
 
 
-function eraser(square) {
-    eraserBtn.addEventListener('click', function () {
-        square.addEventListener('mouseover', () => {
-            square.style.cssText = 'border-top: 1px solid rgb(160, 160, 160); border-left: 1px solid rgb(160, 160, 160);background: white;';
-        });
-    });
-}
+function clearGrid() {
+    container.innerHTML = '';
+};
 
-function colorMode(square) {
-    colorBtn.addEventListener('click', function () {
-        square.addEventListener('mouseover', () => {
-            square.style.cssText = 'border-top: 1px solid rgb(160, 160, 160); border-left: 1px solid rgb(160, 160, 160);background-color: red;';
-        });
-    });
-}
+// ------ RANDOM COLOR -FUNCTION-----//
 
-function bwMode(square) {
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+};
+
+// ------ BLACK & WHITE FUNCTION-----//
+
+function bwMode(pixel) {
     bwBtn.addEventListener('click', function () {
-        square.addEventListener('mouseover', () => {
-            square.style.cssText = 'border-top: 1px solid rgb(160, 160, 160); border-left: 1px solid rgb(160, 160, 160);background-color: rgb(50, 50, 50);';
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = "rgb(50, 50, 50)"
         });
     });
-}
+};
+
+
+
+// ------ RAINBOW MODE FUNCTION-----//
+
+function colorMode(pixel) {
+    colorBtn.addEventListener('click', function () {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = randomColor();
+        });
+    });
+};
+
+// ------ PENCIL MODE FUNCTION-----//
+
+// function pencil(pixel) {
+//     pencilMode.addEventListener('click', function () {
+//         pixel.addEventListener('mouseover', () => {
+//             pixel.style.backgroundColor = randomColor();
+//         });
+//     });
+// };
+
+// ------ ERASER -FUNCTION-----//
+
+function eraser(pixel) {
+    eraserBtn.addEventListener('click', function () {
+        pixel.addEventListener('mouseover', () => {
+            pixel.style.backgroundColor = "white"
+        });
+    });
+};
 
 
